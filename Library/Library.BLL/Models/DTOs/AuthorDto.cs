046ed64d8c91afc701a5fd6fcc54691545ee87f3
Denis Konchik <denis.pptx@gmail.com>
2024-01-27 21:51:28 +0300

    AuthorDto with AuthorValidator created

---
+++ Library/Library.BLL/Models/DTOs/AuthorDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Library.BLL.Models.DTO;
+
+public class AuthorDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ Library/Library.BLL/Models/Validators/AuthorValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Library.BLL.Models.DTO;
+
+namespace Library.BLL.Models.Validators;
+
+public class AuthorValidator : AbstractValidator<AuthorDto>
+{
+    public AuthorValidator()
+    {
+        RuleFor(a => a.Name)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(50);
+    }
+}
+
