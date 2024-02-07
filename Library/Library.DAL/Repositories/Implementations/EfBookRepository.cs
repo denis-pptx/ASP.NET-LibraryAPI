5ead338102cff817b0f67bf6e566fb405ae6294b
Denis Konchik <denis.pptx@gmail.com>
2024-02-07 20:08:16 +0300

    relocated '_included' in BookRepo

--- Library/Library.DAL/Repositories/Implementations/EfBookRepository.cs
+++ Library/Library.DAL/Repositories/Implementations/EfBookRepository.cs
@@ -7,15 +7,11 @@ public class EfBookRepository : IRepository<Book>
 {
     protected readonly ApplicationDbContext _dbContext;
     protected readonly DbSet<Book> _books;
-    protected readonly IIncludableQueryable<Book, Author> _included;
+
     public EfBookRepository(ApplicationDbContext dbContext)
     {
         _dbContext = dbContext;
         _books = dbContext.Set<Book>();
-
-        _included = dbContext.Set<Book>()
-            .Include(b => b.Genre)
-            .Include(b => b.Author)!;
     }
 
     public async Task<bool> IsExistsAsync(int id)
@@ -25,17 +21,29 @@ public class EfBookRepository : IRepository<Book>
 
     public async Task<Book> GetByIdAsync(int id)
     {
-        return await _included.SingleAsync(e => e.Id == id);
+        var booksIncluded = _books
+            .Include(b => b.Genre)
+            .Include(b => b.Author)!;
+
+        return await booksIncluded.SingleAsync(e => e.Id == id);
     }
 
     public async Task<IEnumerable<Book>> GetAsync()
     {
-        return await _included.ToListAsync();
+        var booksIncluded = _books
+            .Include(b => b.Genre)
+            .Include(b => b.Author)!;
+
+        return await booksIncluded.ToListAsync();
     }
 
     public async Task<IEnumerable<Book>> GetAsync(Func<Book, bool> filter)
     {
-        return await Task.Run(() => _included.Where(filter));
+        var booksIncluded = _books
+            .Include(b => b.Genre)
+            .Include(b => b.Author)!;
+
+        return await Task.Run(() => booksIncluded.Where(filter));
     }
 
     public async Task<Book> AddAsync(Book book)
@@ -43,7 +51,11 @@ public class EfBookRepository : IRepository<Book>
         await _books.AddAsync(book);
         await _dbContext.SaveChangesAsync();
 
-        return await _included.SingleAsync(b => b.Id == book.Id);
+        var booksIncluded = _books
+            .Include(b => b.Genre)
+            .Include(b => b.Author)!;
+
+        return await booksIncluded.SingleAsync(b => b.Id == book.Id);
     }
 
     public async Task<Book> UpdateAsync(Book book)
@@ -51,7 +63,11 @@ public class EfBookRepository : IRepository<Book>
         await Task.Run(() => _books.Update(book));
         await _dbContext.SaveChangesAsync();
 
-        return await _included.SingleAsync(b => b.Id == book.Id);
+        var booksIncluded = _books
+            .Include(b => b.Genre)
+            .Include(b => b.Author)!;
+
+        return await booksIncluded.SingleAsync(b => b.Id == book.Id);
     }
 
     public async Task DeleteAsync(Book book)
@@ -62,11 +78,19 @@ public class EfBookRepository : IRepository<Book>
 
     public async Task<Book?> FirstOrDefaultAsync(Func<Book, bool> filter)
     {
-        return await Task.Run(() => _included.FirstOrDefault(filter));
+        var booksIncluded = _books
+            .Include(b => b.Genre)
+            .Include(b => b.Author)!;
+
+        return await Task.Run(() => booksIncluded.FirstOrDefault(filter));
     }
 
     public async Task<Book?> SingleOrDefaultAsync(Func<Book, bool> filter)
     {
-        return await Task.Run(() => _included.SingleOrDefault(filter));
+        var booksIncluded = _books
+            .Include(b => b.Genre)
+            .Include(b => b.Author)!;
+
+        return await Task.Run(() => booksIncluded.SingleOrDefault(filter));
     }
 }
